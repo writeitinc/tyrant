b3ca5cdfec7dcd5ba712b1acd412b6b7359563df
writeitinc <writeitinc99@gmail.com>
2023-04-24 03:30:04 -0600

    add shared library versioning & soft links to install script

--- linux-install.sh
+++ linux-install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-DEST=$1
+DEST=${1%/} # strip trailing slash
 
-install -Dm755 -t "$DEST/usr/lib/" "build/lib/libtyrant.so"
-install -Dm644 -t "$DEST/usr/lib/" "build/lib/libtyrant.a"
-install -Dm644 -t "$DEST/usr/include/tyrant/" "build/include/tyrant/tyrant.h"
-install -Dm644 -t "$DEST/usr/share/licenses/tyrant/" "LICENSE"
-install -Dm644 -t "$DEST/usr/share/doc/tyrant/" "README.md"
+VERSION='0.1.0'
+VERSION_MAJOR=${VERSION%%.*}
+
+install -v -Dm755 "build/lib/libtyrant.so" "$DEST/usr/lib/libtyrant.so.$VERSION"
+ln -v -sn "libtyrant.so.$VERSION" "$DEST/usr/lib/libtyrant.so.$VERSION_MAJOR"
+ln -v -sn "libtyrant.so.$VERSION_MAJOR" "$DEST/usr/lib/libtyrant.so"
+
+install -v -Dm644 -t "$DEST/usr/lib/" "build/lib/libtyrant.a"
+install -v -Dm644 -t "$DEST/usr/include/tyrant/" "build/include/tyrant/tyrant.h"
+install -v -Dm644 -t "$DEST/usr/share/licenses/tyrant/" "LICENSE"
+install -v -Dm644 -t "$DEST/usr/share/doc/tyrant/" "README.md"
